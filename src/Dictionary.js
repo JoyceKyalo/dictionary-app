cc464de677da8aebf398597c9c5f0f328b152661
JoyceKyalo <107515318+JoyceKyalo@users.noreply.github.com>
2023-06-28 23:18:57 +0300

    Integrated API call
    Used SheCodes Dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,22 +1,34 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default function Dictionary(){
-    let [keyword, setKeyword] =  useState(null);
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState(null);
 
-    function search(event){
-        event.preventDefault();
-        alert(`Searching for ${keyword} definition`);
-    }
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
 
-    function updateKeyword(event){
-        setKeyword(event.target.value);
-    }
-    return(
-        <div>
-            <h2>Dictionary</h2>
-            <form onSubmit={search}>
-                <input type="search" placeholder="Type a word..." onChange={updateKeyword} />
-            </form>
-        </div>
-    );
+  function search(event) {
+    event.preventDefault();
+    // documentation for API: https://www.shecodes.io/learn/apis/dictionary
+    let apiKey = "0534b4bf77df82f13f307a8bcbtddo1d";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  return (
+    <div>
+      <h2>Dictionary</h2>
+      <form onSubmit={search}>
+        <input
+          type="search"
+          placeholder="Type a word..."
+          onChange={updateKeyword}
+        />
+      </form>
+    </div>
+  );
 }
